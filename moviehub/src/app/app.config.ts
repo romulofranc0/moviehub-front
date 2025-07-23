1dc444ba95bb4663f8bb4e0e3a54ded63012eab1
romulofranc0 <rfsvm97@gmail.com>
2025-07-23 18:13:49 -0300

    providers

--- moviehub/src/app/app.config.ts
+++ moviehub/src/app/app.config.ts
@@ -3,6 +3,9 @@ import {provideAnimationsAsync} from '@angular/platform-browser/animations/async
 import {providePrimeNG} from 'primeng/config';
 import {definePreset} from '@primeng/themes';
 import Aura from '@primeng/themes/aura';
+import {routes} from './app.routes';
+import {provideRouter} from '@angular/router';
+import {provideHttpClient} from '@angular/common/http';
 
 const MyPreset = definePreset(Aura, {
   primitive: {
@@ -664,6 +667,8 @@ const MyPreset = definePreset(Aura, {
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
+    provideRouter(routes),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
