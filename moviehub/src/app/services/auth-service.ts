9ef581ddbaace2368070fdcb00b6968c077ef68c
romulofranc0 <rfsvm97@gmail.com>
2025-07-23 18:42:00 -0300

    adding login function and saving response token

--- moviehub/src/app/services/auth-service.ts
+++ moviehub/src/app/services/auth-service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, tap} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {CreateUserRequest} from '../models/create-user-request';
+import {LoginUserRequest} from '../models/login-user-request';
 
 @Injectable({
   providedIn: 'root'
@@ -17,4 +18,20 @@ export class AuthService {
   }
 
 
+  loginUser(userRequest: LoginUserRequest) {
+
+    return this.http.post<{token:string}>('http://localhost:8080/api/auth/login', userRequest).pipe(
+      tap(response => {
+        localStorage.setItem('token', response.token);
+      })
+    )
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('auth_token');
+  }
+
+  logout() {
+    localStorage.removeItem('auth_token');
+  }
 }
