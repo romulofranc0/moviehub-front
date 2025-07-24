7ef62c95a963ef9b856811f9ce0e312166d7c75b
romulofranc0 <rfsvm97@gmail.com>
2025-07-24 20:46:13 -0300

    add movie-search path

--- moviehub/src/app/app.routes.ts
+++ moviehub/src/app/app.routes.ts
@@ -2,20 +2,27 @@ import { Routes } from '@angular/router';
 import {Home} from './pages/home/home';
 import {Login} from './pages/login/login';
 import {Register} from './pages/register/register';
+import {MovieSearch} from './pages/movie-search/movie-search';
 
 export const routes: Routes = [
   {
     path:"",
     component:Home
-  },{
+  },
+  {
   path:"login",
   component:Login
   },
   {
     path:"register",
     component:Register
-  },  {
+  },
+  {
     path:"home",
     component:Home
+  },
+  {
+    path:"movie-search/:title",
+    component:MovieSearch
   }
 ];
