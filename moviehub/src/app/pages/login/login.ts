35f77403ba39e0894d2e39d3ad78172ff8810818
romulofranc0 <rfsvm97@gmail.com>
2025-07-24 20:49:02 -0300

    small login page improvements

--- moviehub/src/app/pages/login/login.ts
+++ moviehub/src/app/pages/login/login.ts
@@ -1,37 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {Button} from "primeng/button";
 import {Card} from "primeng/card";
 import {InputText} from "primeng/inputtext";
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {Router, RouterLink} from "@angular/router";
 import {Toast} from "primeng/toast";
-import {CreateUserRequest} from '../../models/create-user-request';
 import {MessageService} from 'primeng/api';
 import {AuthService} from '../../services/auth-service';
 import {LoginUserRequest} from '../../models/login-user-request';
+import {Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-login',
-    imports: [
-        Button,
-        Card,
-        InputText,
-        ReactiveFormsModule,
-        RouterLink,
-        Toast
-    ],
+  imports: [
+    Button,
+    Card,
+    InputText,
+    ReactiveFormsModule,
+    RouterLink,
+    Toast
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
-  loginForm: FormGroup;
-  userRequest?:LoginUserRequest;
+  private formBuilder = inject(FormBuilder);
+  private _messageService = inject(MessageService);
+  private _authService = inject(AuthService);
+  private router = inject(Router);
 
-  constructor(private formBuilder: FormBuilder,
-              private _messageService:MessageService,
-              private _authService:AuthService,
-              private router:Router) {
-    this.loginForm = formBuilder.group({
+  loginForm: FormGroup;
+  userRequest?: LoginUserRequest;
+
+  constructor() {
+    this.loginForm = this.formBuilder.group({
       username: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     })
@@ -40,20 +41,20 @@ export class Login {
   onSubmit() {
     if (this.loginForm.valid) {
       this.userRequest = {
-        username:this.loginForm.value.username,
-        password:this.loginForm.value.password,
+        username: this.loginForm.value.username,
+        password: this.loginForm.value.password,
       }
       this._authService.loginUser(this.userRequest).subscribe({
-        next: result => {
-          this._messageService.add({ severity: 'success', summary: 'Success', detail: 'Login successfully' });
+        next: () => {
+          this._messageService.add({severity: 'success', summary: 'Success', detail: 'Login successfully'});
           this.router.navigate(['/home']);
         },
-        error: err => {
-          this._messageService.add({ severity: 'error', summary: 'Error', detail: err.error?.message });
+        error: (err: { error: { message: any; }; }) => {
+          this._messageService.add({severity: 'error', summary: 'Error', detail: err.error?.message});
         }
       })
 
-    }else{
+    } else {
 
     }
   }
