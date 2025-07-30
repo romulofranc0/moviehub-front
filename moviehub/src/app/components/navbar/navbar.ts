302cec425e065077afd89cde5889cdeb361bede3
romulofranc0 <rfsvm97@gmail.com>
2025-07-30 07:53:11 -0300

    checking if user is logged and changing buttons accordingly

--- moviehub/src/app/components/navbar/navbar.ts
+++ moviehub/src/app/components/navbar/navbar.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {ButtonModule} from 'primeng/button';
 import {RouterLink} from '@angular/router';
+import {AuthService} from '../../services/auth-service';
 
 @Component({
   selector: 'app-navbar',
@@ -12,5 +13,16 @@ import {RouterLink} from '@angular/router';
   styleUrl: './navbar.scss'
 })
 export class Navbar {
+  isLoggedIn = false;
+
+  constructor(private authService: AuthService) {
+    this.authService.isLoggedIn$.subscribe(value => {
+      this.isLoggedIn = value;
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 
 }
