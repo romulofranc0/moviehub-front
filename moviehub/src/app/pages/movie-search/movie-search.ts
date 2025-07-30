bba78c0880f6ed044533ad570025eaddb85801a2
romulofranc0 <rfsvm97@gmail.com>
2025-07-30 07:51:50 -0300

    making movie search function and iterate over movies with @for

--- moviehub/src/app/pages/movie-search/movie-search.ts
+++ moviehub/src/app/pages/movie-search/movie-search.ts
@@ -1,6 +1,8 @@
-import {Component, signal} from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {MovieSearchCard} from '../../components/movie-search-card/movie-search-card';
 import {MovieSearchResponse} from '../../models/movie-search-response';
+import {MovieService} from '../../services/movie-service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-movie-search',
@@ -10,8 +12,25 @@ import {MovieSearchResponse} from '../../models/movie-search-response';
   templateUrl: './movie-search.html',
   styleUrl: './movie-search.scss'
 })
-export class MovieSearch {
-  private _movies = signal<MovieSearchResponse[]>([]);
+export class MovieSearch implements OnInit {
+  private _movieService = inject(MovieService)
+  private _route = inject(ActivatedRoute);
+
+
+  public movieSearchResults: MovieSearchResponse[] = [];
+
+  ngOnInit(): void {
+    this._route.params.subscribe(params => {
+      this._movieService.searchMovie(params['title']).subscribe({
+        next: result => {
+          this.movieSearchResults = result;
+        }
+        }
+
+      )
+    })
+  }
+
 
 
 
