95a84f2b0a77863b47fc4130db03919fe2e2def1
romulofranc0 <rfsvm97@gmail.com>
2025-07-30 07:51:04 -0300

    movie details request when in MovieDetails

--- moviehub/src/app/pages/movie-details/movie-details.ts
+++ moviehub/src/app/pages/movie-details/movie-details.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {MovieDetailsCard} from '../../components/movie-details-card/movie-details-card';
+import {ActivatedRoute} from '@angular/router';
+import {MovieService} from '../../services/movie-service';
+import {MovieDetailsResponse} from '../../models/movie-details-response';
 
 @Component({
   selector: 'app-movie-details',
@@ -9,6 +12,20 @@ import {MovieDetailsCard} from '../../components/movie-details-card/movie-detail
   templateUrl: './movie-details.html',
   styleUrl: './movie-details.scss'
 })
-export class MovieDetails {
+export class MovieDetails implements OnInit {
+  private _movieService = inject(MovieService);
+  private _route = inject(ActivatedRoute);
+  movie!: MovieDetailsResponse;
+  imdbId!: string;
+
+  ngOnInit() {
+    this.imdbId = this._route.snapshot.paramMap.get('imdbId')!;
+    this._movieService.getMovieDetails(this.imdbId).subscribe({
+      next: (result) => {
+        this.movie = result;
+      }
+    })
+  }
+
 
 }
