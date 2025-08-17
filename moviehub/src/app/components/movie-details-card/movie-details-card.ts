4d711a79f7a0391fecb5a30c896ab9673d19edbe
romulofranc0 <rfsvm97@gmail.com>
2025-08-17 10:37:13 -0300

    adding enableReview

--- moviehub/src/app/components/movie-details-card/movie-details-card.ts
+++ moviehub/src/app/components/movie-details-card/movie-details-card.ts
@@ -1,24 +1,31 @@
-import {Component, inject, Input} from '@angular/core';
+import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
 import {Card} from 'primeng/card';
 import {Button} from 'primeng/button';
 import {Rating} from 'primeng/rating';
 import {FormsModule} from '@angular/forms';
 import {MovieDetailsResponse} from '../../models/movie-details-response';
-import {MovieService} from "../../services/movie-service";
-import {MovieDetails} from "../../pages/movie-details/movie-details";
+import {ActivatedRoute, Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-movie-details-card',
   imports: [
     Card,
     Button,
-    Rating,
     FormsModule
   ],
   templateUrl: './movie-details-card.html',
   styleUrl: './movie-details-card.scss'
 })
 export class MovieDetailsCard {
+  private _router= inject(Router);
+  private _activeRoute = inject(ActivatedRoute);
+
   @Input() movie!: MovieDetailsResponse;
+  @Output() reviewEnabled = new EventEmitter<boolean>();
+
+  enableReviewMode() {
+    this.reviewEnabled.emit(true);
+  }
 
 }
